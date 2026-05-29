76c72ebe64714f96959405bd48cafdfe4a8fd2e0
Brian2kf <brianjonatan95@gmail.com>
2026-05-30 01:08:46 +0700

    docs: add project description documentation and remove test scripts

--- test2.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'config/database.php';
-$db = Database::getInstance();
-$stmt = $db->prepare('SELECT * FROM products WHERE id = ?');
-$stmt->execute([25]);
-print_r($stmt->fetch(PDO::FETCH_ASSOC));
